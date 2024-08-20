cc39749b9211359b69106b39dbd705c8dc98de1b
Yedidya Newlander <ydidya.n@gmail.com>
2024-08-20 10:15:00 +0300

    buggy path

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="relative p-4 md:p-8 lg:p-16">
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-b from-blue-500 to-slate-200 z-[-1]"></div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-[url('/path/to/mesh-bg.svg')] opacity-20 z-[-1]"></div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-20 z-[-1]"></div>
 
       <Nav />
       <Main />
