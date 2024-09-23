1c3c539f32961c3a665370f8bfcb3682f345378c
Didi-Tech <177790331+DiDiTech-IL@users.noreply.github.com>
2024-09-24 00:49:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Projects from "./components/projects";
 import Tech from "./components/tech";
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased" dir="rtl">
+    <div className="overflow-x-hidden text-slate-300 antialiased" dir="rtl">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
