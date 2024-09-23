861c00f368b34fee59962f3f5cf7ccd646031805
Didi-Tech <177790331+DiDiTech-IL@users.noreply.github.com>
2024-09-23 14:41:12 +0300

    Another fix cta-button.tsx

--- src/components/cta-button.tsx
+++ src/components/cta-button.tsx
@@ -1,6 +1,6 @@
 import { Button } from "./ui/button"
 
-const CTAButton: React.FC<{ children: React.ReactNode, type?: <"default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "gooeyRight" | null | undefined> }> = ({ children, type = null }) => {
+const CTAButton: React.FC<{ children: React.ReactNode, type?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link" | "gooeyRight" | null | undefined}> = ({ children, type = null }) => {
 
     return (
         <Button asChild variant={type ? type : "gooeyRight"} className={` ${type && "text-white"} transition-all duration-300 hover:bg-primary/70 hover:ring-2 hover:ring-primary/70 hover:ring-offset-1 z-10`}>
